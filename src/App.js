b3983d1ebb03af4d494f87be882560b8aa689ce0
Tom Brandon <tom.brandon@rbs.co.uk>
2021-01-03 22:55:58 +0000

    Added extra days. Created icon for showing previous days

--- src/App.js
+++ src/App.js
@@ -56,6 +56,41 @@ function App() {
             <p class="previous-day__temp">Low: -180°C</p>
             <button class="previous-day__more-info">more info</button>
           </div>
+          <div class="previous-day">
+            <h3 class="previous-day__sol">Sol 371</h3>
+            <p class="previous-day__date">September 31</p>
+            <p class="previous-day__temp">High: -20°C</p>
+            <p class="previous-day__temp">Low: -180°C</p>
+            <button class="previous-day__more-info">more info</button>
+          </div>
+          <div class="previous-day">
+            <h3 class="previous-day__sol">Sol 371</h3>
+            <p class="previous-day__date">September 31</p>
+            <p class="previous-day__temp">High: -20°C</p>
+            <p class="previous-day__temp">Low: -180°C</p>
+            <button class="previous-day__more-info">more info</button>
+          </div>
+          <div class="previous-day">
+            <h3 class="previous-day__sol">Sol 371</h3>
+            <p class="previous-day__date">September 31</p>
+            <p class="previous-day__temp">High: -20°C</p>
+            <p class="previous-day__temp">Low: -180°C</p>
+            <button class="previous-day__more-info">more info</button>
+          </div>
+          <div class="previous-day">
+            <h3 class="previous-day__sol">Sol 371</h3>
+            <p class="previous-day__date">September 31</p>
+            <p class="previous-day__temp">High: -20°C</p>
+            <p class="previous-day__temp">Low: -180°C</p>
+            <button class="previous-day__more-info">more info</button>
+          </div>
+          <div class="previous-day">
+            <h3 class="previous-day__sol">Sol 371</h3>
+            <p class="previous-day__date">September 31</p>
+            <p class="previous-day__temp">High: -20°C</p>
+            <p class="previous-day__temp">Low: -180°C</p>
+            <button class="previous-day__more-info">more info</button>
+          </div>
         </div>
       </div>
     </div>
