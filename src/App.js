0a82bb0566c6c9dd23aae254a27f35822326677e
Tom Brandon <tom.brandon@rbs.co.uk>
2021-01-04 13:42:06 +0000

    Previous weather title position change during animation

--- src/App.js
+++ src/App.js
@@ -38,15 +38,12 @@ function App() {
         </div>
       </main>
 
-      <div class="previous-weather show-weather">
+      <div class="previous-weather">
         <button for="weather-toggle" class='show-previous-weather'>
           <span> &#8593;</span>
           <span class="sr-only">Show previous weather</span>
         </button>
-
-        <input type="checkbox" id="weather-toggle" class="show-previous-weather-toggle" />
-
-        <h2 class="main-title">Previous 7 days</h2>
+        <h2 class="main-title previous-weather__title">Previous 7 days</h2>
 
         <div class="previous-days">
           <div class="previous-day">
