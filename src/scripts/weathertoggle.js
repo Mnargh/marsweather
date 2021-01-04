b87c7c4648c1a790961a4baccd684418516012b7
Tom Brandon <tom.brandon@rbs.co.uk>
2021-01-04 19:25:23 +0000

    Started api call in weathertoggle script

--- src/scripts/weathertoggle.js
+++
@@ -1,9 +0,0 @@
-const previousWeatherToggle = document.querySelector('.show-previous-weather');
-
-const previousWeather = document.querySelector('.previous-weather');
-
-previousWeatherToggle.addEventListener('click', () => {
-  previousWeather.classList.toggle('.show-weather');
-});
-
-console.log('hello');
