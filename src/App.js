d110725e76fcacc6ee00fb94e72b96fae107328b
Tom Brandon <tom.brandon@rbs.co.uk>
2021-01-04 21:03:17 +0000

    Created current weather component

--- src/App.js
+++ src/App.js
@@ -3,40 +3,7 @@ import './styling/main.scss';
 function App() {
   return (
     <div className="App">
-      <main class="mars-current-weather" >
-        <h1 class="main-title">Latest weather at Elysium Plantitia</h1>
-        <div class="date">
-          <h2 class="section-title section-title--date">Sol 377</h2>
-          <p class="date__day">September 18</p>
-        </div>
-        <div class="temperature">
-          <h2 class="section-title">Temperature</h2>
-          <p class="reading">High: -20°C</p>
-          <p class="reading">Low: -180°C</p>
-        </div>
-        <div class="wind">
-          <h2 class="section-title">Wind</h2>
-          <p class="reading">75 kph</p>
-
-          <div class="wind__direction">
-            <p class="sr-only">45 degrees</p>
-            <div class="wind__arrow"></div>
-          </div>
-        </div>
-        <div class="info">
-          <p>Insight is taking daily weather measurements (temperature, wind, pressure) on the surface of Mars at Elysium Plantitia,
-          a flat smooth crater near Mars' equator</p>
-
-          <p>This is only part of insights mission. <a href="https://mars.nasa.gov/insight/mission/overview">Click here</a> to find out more</p>
-        </div>
-        <div class="unit">
-          <label for="cel">°C</label>
-          <input type="radio" id="cel" name="unit" checked />
-          <button class="unit__toggle" />
-          <label for="fah">°F</label>
-          <input type="radio" id="fah" name="unit" />
-        </div>
-      </main>
+      
 
       <div class="previous-weather">
         <button for="weather-toggle" class='show-previous-weather'>
