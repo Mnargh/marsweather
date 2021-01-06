da9c65355d6c098f85d671e8e0e3c9b141764306
Tom Brandon <tom.brandon@rbs.co.uk>
2021-01-06 10:08:06 +0000

    Added API call and displaying data in current weather component

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const CurrentWeather = props => {
+const CurrentWeather = ({ weather: {
+  sol,
+  maxTemp,
+  minTemp,
+  avWindSpeed,
+  windDirectionDeg,
+  windDirectionCardinal,
+  date
+} }) => {
+
   return (
     <main className="mars-current-weather" >
       <h1 className="main-title">Latest weather at Elysium Plantitia</h1>
       <div className="date">
-        <h2 className="section-title section-title--date">Sol 377</h2>
-        <p className="date__day">September 18</p>
+        <h2 className="section-title section-title--date">
+          Sol {sol}</h2>
+        <p className="date__day"> {date}</p>
       </div>
       <div className="temperature">
         <h2 className="section-title">Temperature</h2>
-        <p className="reading">High: -20°C</p>
-        <p className="reading">Low: -180°C</p>
+        <p className="reading">High: {maxTemp}°C</p>
+        <p className="reading">Low: -{minTemp}°C</p>
       </div>
       <div className="wind">
         <h2 className="section-title">Wind</h2>
-        <p className="reading">75 kph</p>
+        <p className="reading"> {avWindSpeed} kph</p>
 
         <div className="wind__direction">
-          <p className="sr-only">45 degrees</p>
+          <p className="sr-only">{windDirectionDeg} degrees</p>
           <div className="wind__arrow"></div>
         </div>
       </div>
@@ -41,7 +51,7 @@ const CurrentWeather = props => {
 }
 
 CurrentWeather.propTypes = {
-
+  weatherData: PropTypes.object.isRequired,
 }
 
 export default CurrentWeather
--- src/components/PreviousWeather.js
+++ src/components/PreviousWeather.js
@@ -1,40 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import PreviousDay from './PreviousDay'
 
 const PreviousWeather = props => {
-
-  const NASA_API_KEY = process.env.REACT_APP_NASA_API_KEY
-
-  const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${NASA_API_KEY}&feedtype=json&ver=1.0`
-
-  useEffect(() => {
-    fetch(API_URL)
-      .then(res => res.json())
-      .then(data => {
-        const {
-          sol_keys,
-          validity_checks,
-          ...solData // gets remaining data
-        } = data
-
-        const temp = Object.entries(solData).map(
-          ([sol, data]) => {
-
-            const pickedData = {
-              ...(sol ? { sol: sol } : { sol: null }),
-              ...(data.AT ? { maxTemp: data.AT.mx } : { maxTemp: null }),
-              ...(data.AT ? { minTemp: data.AT.mn } : { minTemp: null }),
-              ...(data.HWS ? { avWindSpeed: data.HWS.av } : { avWindSpeed: null }),
-              ...(data.WD.most_common ? { windDirectionDeg: data.WD.most_common.compass_degrees } : { windDirectionDeg: null }),
-              ...(data.WD.most_common ? { windDirectionCardinal: data.WD.most_common.compass_point } : { windDirectionCardinal: null }),
-              ...(data.First_UTC ? { date: new Date(data.First_UTC) } : { date: null }),
-            }
-            return pickedData;
-          }
-        )
-      })
-  }, [])
 
   const [showWeather, setShowWeather] = useState('false');
 
